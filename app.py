00f403604f98d07b2204409ae881fe9c6f4b9c44
Woody <gupeng2015@gmail.com./run.sh>
2019-04-01 10:24:38 -0500

    try add nltk txt file

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask
-# import readpdf
+import readpdf
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World!"
-#
-# @app.route('/api/text/<filename>', methods=['GET'])
-# def get_pdf_content(filename):
-#     file_path = './resume/' + filename
-#     return readpdf.convert_pdf_to_txt(file_path)
+
+@app.route('/api/text/<filename>', methods=['GET'])
+def get_pdf_content(filename):
+    file_path = './resume/' + filename
+    return readpdf.convert_pdf_to_txt(file_path)
 
 if __name__ == '__main__':
     app.run()
