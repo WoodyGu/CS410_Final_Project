93b8ea4bef6a9ddbbf7e20c4ced41e00b84bccf4
Woody <gupeng2015@gmail.com./run.sh>
2019-04-13 16:02:04 -0500

    added send file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from flask import send_file
 import gunicorn
 import readpdf
 import utils
@@ -24,6 +25,14 @@ def rank_documents(query):
     pdf_file_list = os.listdir('./resume/')
     candidate_list = utils.parse_candidate(pdf_file_list)
     return json.dumps(candidate_list)
+
+@app.route('/api/file/<filename>')
+def get_pdf_file(filename):
+    file_path = './resume/' + filename
+    try:
+        return send_file(file_path,attachment_filename=filename)
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
     app.run()
