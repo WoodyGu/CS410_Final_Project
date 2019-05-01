9a044faf7d6b74e968bdf28bd92ea6e07fc360f8
Woody <gupeng2015@gmail.com./run.sh>
2019-04-30 21:59:53 -0500

    added documentation

--- app.py
+++ app.py
@@ -18,11 +18,37 @@ def hello():
 
 @app.route('/api/text/<filename>', methods=['GET'])
 def get_pdf_content(filename):
+    """returns the text content of a pdf file stored in back-end
+
+    Parameters
+    ----------
+    filename : str
+        Description of parameter `filename`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     file_path = './resume/' + filename
     return readpdf.convert_pdf_to_txt(file_path)
 
 @app.route('/api/ranking/<query>', methods=['GET'])
 def rank_documents(query):
+    """returns a list of information of qualified(relevant) candidates(key function)
+
+    Parameters
+    ----------
+    query : str
+        Description of parameter `query`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     # pdf_file_list = os.listdir('./resume/')
     with open("queries.txt", 'w') as fd:
         fd.write(str(query))
@@ -36,11 +62,36 @@ def rank_documents(query):
 
 @app.route('/api/file/totalcount', methods=['GET'])
 def get_doc_count():
+    """returns the total number of documents(resumes) stored in the back-end
+
+    Parameters
+    ----------
+
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     all_file = os.listdir('./resume/')
     return json.dumps(len(all_file))
 
 @app.route('/api/file/<filename>')
 def get_pdf_file(filename):
+    """returns the actual pdf file
+
+    Parameters
+    ----------
+    filename : str
+        Description of parameter `filename`.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
     file_path = './resume/' + filename
     try:
         return send_file(file_path,attachment_filename=filename)
